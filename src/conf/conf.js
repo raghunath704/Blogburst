70d81050e7b322901277fc0e3b7d7c88513710be
Raghunath Das <raghunathdas704@gmail.com>
2024-02-03 02:01:04 +0530

    Added Config File

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,3 +1,4 @@
+//these are config variables to access environmental variables
 const conf={
     appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId:String(import.meta.env.VITE_PROJECT_ID),
